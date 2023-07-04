eb8bb11473ecc71e5fd5a6a3952b88b1e4693188
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-07-04 21:45:12 +0300

    BOM example.

---
+++ 06 BOM/main.js
@@ -0,0 +1,36 @@
+// ------------- working with BOM
+window.console.log("Hello BOM!");
+
+// --------- window
+console.log(window);
+console.log(`Window size: ${window.innerHeight}x${window.innerWidth}`);
+
+// --------- history
+console.log(window.history);
+
+//history.go(-3);   // go back 3 pages
+//history.go(3);      // go forward 3 pages
+
+backBtn.onclick = () => history.back();
+forwardBtn.onclick = () => history.forward();
+
+// --------- screen
+console.log(window.screen);
+
+// --------- location
+console.log(location);
+
+w3schoolBtn.onclick = () => {
+    location.assign("https://w3schools.com"); // save history
+    //location.replace("https://w3schools.com"); // remove history
+}
+
+// --------- navigator
+console.log(navigator);
+
+// --------- geolocation
+console.log(navigator.geolocation);
+
+navigator.geolocation.getCurrentPosition((res) => {
+    console.log(`My location: ${res.coords.latitude} : ${res.coords.longitude}`);
+}, () => console.log("Cannot get your location!"));
